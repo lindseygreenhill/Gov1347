d12c454ae0602aacc6c4abff5d06e828656a1a32
lindseygreenhill <lgreenhill@college.harvard.edu>
2020-09-17 13:36:10 -0400

    gt added

--- Gov1347-master/scripts/Economy_post.R
+++ Gov1347-master/scripts/Economy_post.R
@@ -10,7 +10,9 @@
 
 library(tidyverse)
 library(ggplot2)
+library(rsample)
 library(janitor)
+library(gt)
 popvote_df <- read_csv("Gov1347-master/data/popvote_1948-2016.csv")
 economy_df <- read_csv("Gov1347-master/data/econ.csv") 
 local_df <- read_csv("Gov1347-master/data/local.csv") %>%
@@ -26,15 +28,22 @@ data <- popvote_df %>%
   left_join(economy_df, by = "year") %>%
   filter(year >= 1948, quarter == 2, incumbent_party == TRUE) %>%
   select(year, party, winner, pv2p, incumbent, incumbent_party, prev_admin,
-         GDP_growth_qt, GDP_growth_yr, unemployment, stock_close)
+         GDP_growth_qt, GDP_growth_yr, unemployment, stock_close, RDI_growth)
 
-vars <- c("GDP_growth_qt")
+# getting rid of scientific notation
+options(scipen = 999)
+
 
 
 ######## GDP growth qt ###############
 ######################################
 
+cor_GDP_growth_qt <- cor(data$GDP_growth_qt, data$pv2p)
+
 lm_GDP_growth_qt <- lm(pv2p ~ GDP_growth_qt, data = data)
+
+estimate_GDP_growth_qt <- lm_GDP_growth_qt %>% tidy() %>%
+  filter(term == "GDP_growth_qt") %>% pull(estimate)
 
 # checking in sample model error and MSE
 
@@ -57,6 +66,8 @@ outsamp_errors <- sapply(1:1000, function(i){
 mean_outsamp_GDP_growth_qt <- mean(abs(outsamp_errors))
 
 Model_Statistics <- tibble(var = "GDP_growth_qt",
+              cor = cor_GDP_growth_qt,
+              estimate = estimate_GDP_growth_qt,
               r_sq = r_squared_GDP_growth_qt,
               mse = mse_GDP_growth_qt,
               mean_out = mean_outsamp_GDP_growth_qt)
@@ -64,8 +75,13 @@ Model_Statistics <- tibble(var = "GDP_growth_qt",
 ######## GDP growth yr ###############
 ######################################
 
+cor_GDP_growth_yr <- cor(data$GDP_growth_yr, data$pv2p)
+
+
 lm_GDP_growth_yr <- lm(pv2p ~ GDP_growth_yr, data = data)
 
+estimate_GDP_growth_yr <- lm_GDP_growth_yr %>% tidy() %>%
+  filter(term == "GDP_growth_yr") %>% pull(estimate)
 # checking in sample model error and MSE
 
 r_squared_GDP_growth_yr<- summary(lm_GDP_growth_yr)$r.squared
@@ -87,6 +103,8 @@ outsamp_errors <- sapply(1:1000, function(i){
 mean_outsamp_GDP_growth_yr <- mean(abs(outsamp_errors))
 
 row3 <- c(var = "GDP_growth_yr",
+          cor = cor_GDP_growth_yr,
+          estimate = estimate_GDP_growth_yr,
               r_sq = r_squared_GDP_growth_yr,
               mse = mse_GDP_growth_yr,
               mean_out = mean_outsamp_GDP_growth_yr)
@@ -97,7 +115,12 @@ row3 <- c(var = "GDP_growth_yr",
 ######## unemployment ###############
 ######################################
 
+cor_unemployment <- cor(data$unemployment, data$pv2p)
+
 lm_unemployment <- lm(pv2p ~ unemployment, data = data)
+
+estimate_unemployment <- lm_unemployment %>% tidy() %>%
+  filter(term == "unemployment") %>% pull(estimate)
 
 # checking in sample model error and MSE
 
@@ -120,6 +143,8 @@ outsamp_errors <- sapply(1:1000, function(i){
 mean_outsamp_unemployment <- mean(abs(outsamp_errors))
 
 row2 <- c(var = "unemployment",
+          cor = cor_unemployment,
+          estimate = estimate_unemployment,
               r_sq = r_squared_unemployment,
               mse = mse_unemployment,
               mean_out = mean_outsamp_unemployment)
@@ -128,7 +153,12 @@ row2 <- c(var = "unemployment",
 ######## stock close ###############
 ######################################
 
+cor_stock_close <- cor(data$stock_close, data$pv2p)
+
 lm_stock_close <- lm(pv2p ~ stock_close, data = data)
+
+estimate_stock_close <- lm_stock_close %>% tidy() %>%
+  filter(term == "stock_close") %>% pull(estimate)
 
 # checking in sample model error and MSE
 
@@ -151,15 +181,169 @@ outsamp_errors <- sapply(1:1000, function(i){
 mean_outsamp_stock_close <- mean(abs(outsamp_errors))
 
 row4 <- c(var = "stock_close",
+          cor = cor_stock_close,
+          estimate = estimate_stock_close,
               r_sq = r_squared_stock_close,
               mse = mse_stock_close,
               mean_out = mean_outsamp_stock_close)
 
 
+######## RDI growth ###############
+######################################
+
+
+
+data_RDI <- data %>% filter(year >= 1960)
+cor_RDI_growth <- cor(data_RDI$RDI_growth, data_RDI$pv2p)
+
+lm_RDI_growth <- lm(pv2p ~ RDI_growth, data = data_RDI)
+
+estimate_RDI_growth <- lm_RDI_growth %>% tidy() %>%
+  filter(term == "RDI_growth") %>% pull(estimate)
+
+# checking in sample model error and MSE
+
+r_squared_RDI_growth <- summary(lm_RDI_growth)$r.squared
+
+mse_RDI_growth <- sqrt(mean((lm_RDI_growth$model$pv2p - lm_RDI_growth$fitted.values)^2))
+
+## model testing: cross-validation (1000 runs)
+
+outsamp_errors <- sapply(1:1000, function(i){
+  years_outsamp <- sample(data_RDI$year, 8)
+  outsamp_mod <- lm(pv2p ~ RDI_growth,
+                    data_RDI[!(data_RDI$year %in% years_outsamp),])
+  outsamp_pred <- predict(outsamp_mod,
+                          newdata = data_RDI[data_RDI$year %in% years_outsamp,])
+  outsamp_true <- data_RDI$pv2p[data_RDI$year %in% years_outsamp]
+  mean(outsamp_pred - outsamp_true)
+})
+
+mean_outsamp_RDI_growth <- mean(abs(outsamp_errors))
+
+row5 <- c(var = "RDI_growth",
+          cor = cor_RDI_growth,
+          estimate  = estimate_RDI_growth,
+          r_sq = r_squared_RDI_growth,
+          mse = mse_RDI_growth,
+          mean_out = mean_outsamp_RDI_growth)
+
+
 ########## creating stats table #########
 
 Model_Statistics <- Model_Statistics %>%
-  rbind(row2, row3, row4)
+  rbind(row2, row3, row4, row5)
+
+Model_Stats <- Model_Statistics %>%
+  mutate(Correlation = parse_number(cor),
+         Estimate =  parse_number(estimate),
+         R_Sq = parse_number(r_sq),
+         MSE = parse_number(mse),
+         Outsampling_Error = parse_number(mean_out)
+         ) %>%
+  mutate_if(is.numeric, round, digits = 3)  %>%
+  select(-(2:6)) %>%
+  arrange(desc(R_Sq))
+
+stats_gt <- Model_Stats %>%
+  gt() %>%
+  tab_header(title = "Linear Regression Results") %>%
+  cols_label(var = "IV",  R_Sq = "R Sq",
+             Outsampling_Error = "Outsampling Error")
+
+########## Predictions ##########
+
+GDP_new <- economy_df %>%
+  filter(year == 2020, quarter == 2)  %>%
+  select(GDP_growth_qt)
+
+GDP_prediction  <- predict(lm_GDP_growth_qt, GDP_new, interval="prediction")
+
+RDI_new <- economy_df %>%
+  filter(year == 2020, quarter == 1)  %>%
+  select(RDI_growth)
+
+RDI_prediction  <- predict(lm_RDI_growth, RDI_new, interval="prediction")
+
+
+GDP_yr_new <- economy_df %>%
+  filter(year == 2020, quarter == 1)  %>%
+  select(GDP_growth_yr)
+
+GDP_yr_prediction  <- predict(lm_GDP_growth_yr, GDP_yr_new, interval="prediction")
+
+
+########## NYT Graph ##########
+
+Extrap_GDP <- economy_df %>%
+  filter(year >= 1948,  quarter %in%  c(1,2)) %>%
+  mutate(quarter = if_else(quarter== 1, "Q1","Q2")) %>%
+  ggplot(aes(x = year, y = GDP_growth_qt, fill = (GDP_growth_qt > 0))) +
+  facet_wrap(~quarter) +
+  geom_col(show.legend = FALSE) + 
+  theme_classic() +
+  labs(title = "Quarterly GDP Growth 1948-2020",
+       subtitle = "2020 Q2 GDP decline is unprecedented",
+       x = "Year",
+       y = "Quarterly GDP Growth")
+
+######### Q1 GDP ##########
+data_q1 <- popvote_df %>%
+  left_join(economy_df, by = "year") %>%
+  filter(year >= 1948, quarter == 1, incumbent_party == TRUE) %>%
+  select(year, party, winner, pv2p, incumbent, incumbent_party, prev_admin,
+         GDP_growth_qt, GDP_growth_yr, unemployment, stock_close, RDI_growth)
+
+
+
+######## GDP growth qt ###############
+######################################
+
+cor_GDP_growth_qt_1 <- cor(data_q1$GDP_growth_qt, data_q1$pv2p)
+
+lm_GDP_growth_qt_1 <- lm(pv2p ~ GDP_growth_qt, data = data)
+
+estimate_GDP_growth_qt_1 <- lm_GDP_growth_qt_1 %>% tidy() %>%
+  filter(term == "GDP_growth_qt") %>% pull(estimate)
+
+# checking in sample model error and MSE
+
+r_squared_GDP_growth_qt_1<- summary(lm_GDP_growth_qt_1)$r.squared
+
+mse_GDP_growth_qt_1 <- sqrt(mean((lm_GDP_growth_qt_1$model$pv2p - lm_GDP_growth_qt_1$fitted.values)^2))
+
+## model testing: cross-validation (1000 runs)
+
+outsamp_errors <- sapply(1:1000, function(i){
+  years_outsamp <- sample(data_q1$year, 8)
+  outsamp_mod <- lm(pv2p ~ GDP_growth_qt,
+                    data_q1[!(data_q1$year %in% years_outsamp),])
+  outsamp_pred <- predict(outsamp_mod,
+                          newdata = data_q1[data_q1$year %in% years_outsamp,])
+  outsamp_true <- data_q1$pv2p[data_q1$year %in% years_outsamp]
+  mean(outsamp_pred - outsamp_true)
+})
+
+mean_outsamp_GDP_growth_qt_1 <- mean(abs(outsamp_errors))
+
+Model_Statistics_1 <- tibble(var = "GDP_growth_qt_1",
+                           cor = cor_GDP_growth_qt_1,
+                           estimate = estimate_GDP_growth_qt_1,
+                           r_sq = r_squared_GDP_growth_qt_1,
+                           mse = mse_GDP_growth_qt_1,
+                           mean_out = mean_outsamp_GDP_growth_qt_1)
+
+## prediction q1 ######
+
+GDP_new_1 <- economy_df %>%
+  filter(year == 2020, quarter == 1)  %>%
+  select(GDP_growth_qt)
+
+GDP_prediction_1  <- predict(lm_GDP_growth_qt_1, GDP_new_1, interval="prediction")
+
+
+
+
 
 
 ########## states extension ###############3
@@ -360,3 +544,4 @@ economy_df %>%
         plot.title = element_text(size = 12,
                                   hjust = 0.5,
                                   face="bold"))
+
